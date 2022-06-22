a9ab611025044fc81c07fb10574504d4cd6a4ded
BHC-Chicken <tij05078@gmail.com>
2022-06-22 16:41:26 +0900

    init

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-import _ from 'lodash';
+import _, { sample } from 'lodash';
+import axios from 'axios';
+import myData from './myData.json'
 
-console.log('hello world');
+console.log(myData)
+
+const user = {
+    name: 'park',
+    age : 20,
+    emails : [
+        'test@sample.com',
+        'test2@sample.com'
+    ]
+}
+
+console.log('user', user)
+
+const str = JSON.stringify(user)
+console.log('str', str)
+console.log(typeof str)
+
+const obj = JSON.parse(str)
+console.log('obj', obj)
